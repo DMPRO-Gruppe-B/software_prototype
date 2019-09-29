1fef914c69eb3aebc6a3b6bc7c2091aa47801df6
Ole Ravna <olevra@stud.ntnu.no>
2019-09-29 15:59:27 +0200

    Add betterer sine generator

--- music.py
+++ music.py
@@ -14,23 +14,18 @@ def range_compress(array, ratio: float):
 def simple_filter(array, a0 = 0.5, a1 = 0.5):
     return np.array([array[0]] + [s * a0 + s_last * a1 for s, s_last in zip(array[1:], array[:-1])])
 
-def sine(frequency, sampling_rate, num_samples):
-    sine_wave = np.array([2 ** 15 * sin(x * 1000 / sampling_rate) * sin(2 * pi * frequency * x/sampling_rate) for x in range(num_samples)])
-    # plt.plot(sine_wave, linewidth=0.3, alpha=0.7, color='#004bc6')
-    wavfile.write('sine_wave.wav', int(sampling_rate), sine_wave)
-    return sine_wave
-    # plt.savefig("sine_wave.png")
-    # plt.show()
-    
-    
- 
+def geneate_sine_wave(frequency, sampling_rate, num_samples, amplitude = 2**15, save_file=False):
+    sine_wave = np.array([sin(2 * pi * frequency * x/sampling_rate) for x in range(num_samples)])
+    if save_file: 
+        wavfile.write('sine_wave.wav', int(sampling_rate), sine_wave)
+    return amplitude * sine_wave
 
 def generate_wav(fname, sample_rate, data):
 
     wavfile.write(fname, sample_rate, data)
 def plot_frequency(array, new_array, sample_rate):
     
-    frq, X = frequency_spectrum(array, sample_rate)
+    _, X = frequency_spectrum(array, sample_rate)
     _, X2 = frequency_spectrum(new_array, sample_rate)
 
     plt.subplot(2, 1, 2)
